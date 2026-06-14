156264228fcb7d6113bcd602aa2b6ad368439ab1
Nuno Costa <nrfcosta21@gmail.com>
2026-06-14 21:37:49 +0100

    fix downloader to use useragent in requests

--- downloader.py
+++ downloader.py
@@ -2,7 +2,10 @@ import requests
 import shutil
 
 def download_file(url: str, file_path: str) -> None:
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    headers = {
+        "User-Agent": "curl/8.1.2",
+        "Accept": "*/*",
+    }
     with requests.get(url, headers=headers, stream=True) as r:
         try:
             with open(file_path, "wb") as f:
